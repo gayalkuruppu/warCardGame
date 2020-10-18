0e8dd579b059f7c3affc4d7a097dd8523b7b28a7
gayalkuruppu <gayalkuruppu.gk@gmail.com>
2020-10-18 19:55:34 +0530

    added buttons and labels to ViewController

--- warCardGame/ViewController.swift
+++ warCardGame/ViewController.swift
@@ -5,17 +5,31 @@
 //  Created by Gayal Kuruppu on 10/18/20.
 //  Copyright © 2020 mindbyte. All rights reserved.
 //
-// https://www.youtube.com/watch?v=2CsFQZBKGT8 19.00 to adjust size for all classes
+// www.youtube.com/watch?v=2CsFQZBKGT8 19.00 to adjust size for all classes
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var leftImageView: UIImageView!
+    
+    @IBOutlet var rightImageView: UIImageView!
+    
+    
+    @IBOutlet var playerScoreLabel: UILabel!
+    
+    @IBOutlet var cpuScoreLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func dealTapped(_ sender: Any) {
+        
+        print("Deal Tapped")
+        
+    }
+    
 }
 
