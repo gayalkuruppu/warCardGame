3dca7239512c2ba4e60496e0b6e17e4ff26b37e9
gayalkuruppu <gayalkuruppu.gk@gmail.com>
2020-10-18 14:26:14 +0530

    UI first page created

--- warCardGame/ViewController.swift
+++ warCardGame/ViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Gayal Kuruppu on 10/18/20.
 //  Copyright © 2020 mindbyte. All rights reserved.
 //
+// https://www.youtube.com/watch?v=2CsFQZBKGT8 19.00 to adjust size for all classes
 
 import UIKit
 
